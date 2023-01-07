318fa598a3566fd19bfa3c0698e1535577ba59e2
luiz <luiz@sdf.org>
2023-01-07 01:15:39 -0300

    simplify api

--- README.md
+++ README.md
@@ -6,21 +6,20 @@ A Lua wrapper for the [Monocypher](https://monocypher.org/) crypto library.
 
 ### Authenticated encryption with XChaCha20 and Poly1305 ([RFC 8439](https://www.rfc-editor.org/rfc/rfc8439))
 
-    lock(plain_text [, key]) => cipher_text, key
-    unlock(cipher_text, key) => plain_text
+    encrypt(plaintext [, key]) => ciphertext, key
+    decrypt(ciphertext, key) => plaintext
 
-    plain_text: string
-    key: string (32 bytes), randomly generated when not provided
-    cipher_text: nonce string (24 bytes) + string + MAC string (16 bytes)
+    plaintext: string
+    key: string (32 bytes), randomly generated if not provided
+    ciphertext: nonce string (24 bytes) + string + MAC string (16 bytes)
 
-### Hashing with BLAKE2b ([RFC 7693](https://www.rfc-editor.org/rfc/rfc7693))
+### Hashing with BLAKE2 ([RFC 7693](https://www.rfc-editor.org/rfc/rfc7693))
 
-    blake2b(message [, digest_size [, key]]) => digest
+    blake2(message [, key]) => digest
 
     message: string
-    digest_size: number between 1 and 64 (the default is 64)
-    key: string (32 bytes) for a PRF
-    digest: string (digest_size bytes)
+    key: string (32 bytes) for a MAC
+    digest: string (64 bytes)
 
 ### Hashing with SHA-512 ([RFC 6234](https://www.rfc-editor.org/rfc/rfc6234))
 
@@ -31,39 +30,35 @@ A Lua wrapper for the [Monocypher](https://monocypher.org/) crypto library.
 
 ### Password hashing with Argon2 ([RFC 9106](https://www.rfc-editor.org/rfc/rfc9106))
 
-    argon2(password [, salt [, digest_size [, blocks [, iterations]]]]) => digest, salt
+    argon2(password [, salt [, blocks [, iterations]]]) => digest, salt
 
     password: string
-    salt: string (16 bytes), randomly generated when not provided
-    digest_size: number, either 32 or 64 (the default is 32)
+    salt: string (16 bytes), randomly generated if not provided
     blocks: number of kilobytes allocated for the computation (the default is 100000)
     iterations: number (the default is 3)
-    digest: string (digest_size bytes)
+    digest: string (64 bytes)
 
 ### Public key exchanges with X25519 ([RFC 7748](https://www.rfc-editor.org/rfc/rfc7748))
 
-    x25519([their_public_key [, your_secret_key]]) => shared_key, your_public_key, your_secret_key
+    exchange([their_public_key [, your_secret_key]]) => shared_key, your_public_key, your_secret_key
 
     their_public_key: string (32 bytes)
-    your_secret_key: string (32 bytes), randomly generated when not provided
+    your_secret_key: string (32 bytes), randomly generated if not provided
     shared_key: string (32 bytes) if their_public_key else nil
     your_public_key: string (32 bytes)
 
-### Public key signatures ([RFC 8032](https://www.rfc-editor.org/rfc/rfc8032))
+### Public key signatures with Ed25519 ([RFC 8032](https://www.rfc-editor.org/rfc/rfc8032))
 
-    eddsa_sign(message [, secret_key]) => signature, public_key, secret_key
-    ed25519_sign(message [, secret_key]) => signature, public_key, secret_key
-    eddsa_check(message, signature, public_key) => bool
-    ed25519_check(message, signature, public_key) => bool
+    sign(message [, secret_key]) => signature, public_key, secret_key
+    check(message, signature, public_key) => bool
 
     message: string
-    secret_key: string (32 bytes), randomly generated when not provided
-    signature: string (64 bytes)
+    secret_key: string (32 bytes), randomly generated if not provided
     public_key: string (32 bytes)
 
 ### Constant-time comparisons
 
-    memcmp(a, b) => bool
+    equal(a, b) => bool
 
     a: string
     b: string
@@ -73,7 +68,7 @@ A Lua wrapper for the [Monocypher](https://monocypher.org/) crypto library.
     random(size) => buffer
 
     size: number
-    buffer: string
+    buffer: string (size bytes)
 
 ## License
 
--- lua-monocypher.c
+++ lua-monocypher.c
@@ -7,7 +7,7 @@
 #ifdef _WIN32
 #include <bcrypt.h>
 #define PRNG(buffer, size) BCryptGenRandom(NULL, buffer, size, \
-    BCRYPT_USE_SYSTEM_PREFERRED_PRNG)
+                                           BCRYPT_USE_SYSTEM_PREFERRED_PRNG)
 #elif defined(__linux__)
 #include <sys/random.h>
 #define PRNG(buffer, size) getrandom(buffer, size, 0);
@@ -19,13 +19,15 @@
 #define luaL_newlib(L, l) (luaL_register(L, "monocypher", l))
 #endif
 
-static void * l_malloc(lua_State *L, size_t size) {
+static void *l_malloc(lua_State *L, size_t size)
+{
     void *ptr = malloc(size);
     if (!ptr) luaL_error(L, "not enough memory");
     return ptr;
 }
 
-static int l_lock(lua_State *L) {
+static int l_encrypt(lua_State *L)
+{
     size_t text_size, key_size;
     const char *plaintext = luaL_checklstring(L, 1, &text_size);
     char *key = (char *) luaL_optlstring(L, 2, NULL, &key_size);
@@ -41,8 +43,9 @@ static int l_lock(lua_State *L) {
         free_key = 1;
     }
     crypto_lock((uint8_t *) ciphertext + 24 + text_size,
-        (uint8_t *) ciphertext + 24, (const uint8_t *) key,
-        (const uint8_t *) ciphertext, (const uint8_t *) plaintext, text_size);
+                (uint8_t *) ciphertext + 24, (const uint8_t *) key,
+                (const uint8_t *) ciphertext, (const uint8_t *) plaintext,
+                text_size);
 
     lua_pushlstring(L, ciphertext, text_size + 40);
     lua_pushlstring(L, key, 32);
@@ -51,21 +54,22 @@ static int l_lock(lua_State *L) {
     return 2;
 }
 
-static int l_unlock(lua_State *L) {
+static int l_decrypt(lua_State *L)
+{
     size_t text_size, key_size;
     const char *ciphertext = luaL_checklstring(L, 1, &text_size);
     const char *key = luaL_checklstring(L, 2, &key_size);
     char *plaintext;
 
     luaL_argcheck(L, text_size >= 40, 1,
-        "#ciphertext must be at least 40 (nonce + MAC)");
+                  "#ciphertext must be at least 40 (nonce + MAC)");
     luaL_argcheck(L, key_size == 32, 2, "#key must be 32");
     text_size -= 40;
     plaintext = (char *) l_malloc(L, text_size);
     if (crypto_unlock((uint8_t *) plaintext, (const uint8_t *) key,
-        (const uint8_t *) ciphertext,
-        (const uint8_t *) ciphertext + 24 + text_size,
-        (const uint8_t *) ciphertext + 24, text_size) == -1) {
+                      (const uint8_t *) ciphertext,
+                      (const uint8_t *) ciphertext + 24 + text_size,
+                      (const uint8_t *) ciphertext + 24, text_size) == -1) {
         free(plaintext);
         luaL_error(L, "bad ciphertext");
     }
@@ -75,27 +79,23 @@ static int l_unlock(lua_State *L) {
     return 1;
 }
 
-static int l_blake2b(lua_State *L) {
+static int l_blake2(lua_State *L)
+{
     size_t message_size, key_size;
     const char *message = luaL_checklstring(L, 1, &message_size);
-    size_t digest_size = luaL_optinteger(L, 2, 64);
-    const char *key = luaL_optlstring(L, 3, NULL, &key_size);
-    char *digest;
+    const char *key = luaL_optlstring(L, 2, NULL, &key_size);
+    char digest[64];
 
-    luaL_argcheck(L, digest_size >= 1 && digest_size <= 64, 2,
-        "digest_size must be between 1 and 64");
-    luaL_argcheck(L, key_size == 0 || key_size == 32, 3, "#key must be 32");
-    digest = (char *) l_malloc(L, digest_size);
-    crypto_blake2b_general((uint8_t *) digest, digest_size,
-        (const uint8_t *) key, key_size, (const uint8_t *) message,
-        message_size);
+    luaL_argcheck(L, key_size == 0 || key_size == 32, 2, "#key must be 32");
+    crypto_blake2b_general((uint8_t *) digest, 64, (const uint8_t *) key,
+                           key_size, (const uint8_t *) message, message_size);
 
-    lua_pushlstring(L, digest, digest_size);
-    free(digest);
+    lua_pushlstring(L, digest, 64);
     return 1;
 }
 
-static int l_sha512(lua_State *L) {
+static int l_sha512(lua_State *L)
+{
     size_t message_size;
     const char *message = luaL_checklstring(L, 1, &message_size);
     char digest[64];
@@ -106,55 +106,51 @@ static int l_sha512(lua_State *L) {
     return 1;
 }
 
-static int l_argon2(lua_State *L) {
+static int l_argon2(lua_State *L)
+{
     size_t password_size, salt_size;
     const char *password = luaL_checklstring(L, 1, &password_size);
     char *salt = (char *) luaL_optlstring(L, 2, NULL, &salt_size);
     char free_salt = 0;
-    uint32_t digest_size = luaL_optinteger(L, 3, 32);
-    uint32_t nb_blocks = luaL_optinteger(L, 4, 100000);
-    uint32_t nb_iterations = luaL_optinteger(L, 5, 3);
-    char *digest;
+    uint32_t nb_blocks = luaL_optinteger(L, 3, 100000);
+    uint32_t nb_iterations = luaL_optinteger(L, 4, 3);
     void *work_area;
+    char digest[64];
 
-    luaL_argcheck(L, salt_size == 0 || salt_size == 16, 2,
-        "#salt must be 16");
-    luaL_argcheck(L, digest_size == 32 || digest_size == 64, 3,
-        "digest_size must be either 32 or 64");
+    luaL_argcheck(L, salt_size == 0 || salt_size == 16, 2, "#salt must be 16");
     work_area = l_malloc(L, nb_blocks * 1024);
-    digest = (char *) l_malloc(L, digest_size);
     if (!salt) {
         salt = (char *) l_malloc(L, 16);
         PRNG(salt, 16);
         free_salt = 1;
     }
-    crypto_argon2i((uint8_t *) digest, digest_size, work_area, nb_blocks,
-        nb_iterations, (const uint8_t *) password, password_size,
-        (const uint8_t *) salt, 16);
+    crypto_argon2i((uint8_t *) digest, 64, work_area, nb_blocks, nb_iterations,
+                   (const uint8_t *) password, password_size,
+                   (const uint8_t *) salt, 16);
 
-    lua_pushlstring(L, digest, digest_size);
+    lua_pushlstring(L, digest, 64);
     lua_pushlstring(L, salt, 16);
     free(work_area);
-    free(digest);
     if (free_salt) free(salt);
     return 2;
 }
 
-static int l_x25519(lua_State *L) {
+static int l_exchange(lua_State *L)
+{
     size_t their_public_key_size, your_secret_key_size;
     const char *their_public_key = luaL_optlstring(L, 1, NULL,
-        &their_public_key_size);
+                                                   &their_public_key_size);
     char *your_secret_key = (char *) luaL_optlstring(L, 2, NULL,
-        &your_secret_key_size);
+                                                     &your_secret_key_size);
     char free_your_secret_key = 0;
     char your_public_key[32];
     char shared_key[32];
     const uint8_t zero[16] = { 0 };
 
     luaL_argcheck(L, their_public_key_size == 0 || their_public_key_size == 32,
-        1, "#their_public_key must be 32");
-    luaL_argcheck(L, your_secret_key_size == 0 || your_secret_key_size == 32, 2,
-        "#your_secret_key must be 32");
+                  1, "#their_public_key must be 32");
+    luaL_argcheck(L, your_secret_key_size == 0 || your_secret_key_size == 32,
+                  2, "#your_secret_key must be 32");
     if (!your_secret_key) {
         your_secret_key = (char *) l_malloc(L, 32);
         PRNG(your_secret_key, 32);
@@ -162,15 +158,15 @@ static int l_x25519(lua_State *L) {
     }
     if (their_public_key) {
         crypto_x25519((uint8_t *) shared_key, (const uint8_t *)
-            your_secret_key, (const uint8_t *) their_public_key);
+                      your_secret_key, (const uint8_t *) their_public_key);
         crypto_hchacha20((uint8_t *) shared_key, (const uint8_t *) shared_key,
-            zero);
+                         zero);
         lua_pushlstring(L, shared_key, 32);
     } else {
         lua_pushnil(L);
     }
-    crypto_x25519_public_key((uint8_t *) your_public_key, (const uint8_t *)
-        your_secret_key);
+    crypto_x25519_public_key((uint8_t *) your_public_key,
+                             (const uint8_t *) your_secret_key);
 
     lua_pushlstring(L, your_public_key, 32);
     lua_pushlstring(L, your_secret_key, 32);
@@ -178,7 +174,8 @@ static int l_x25519(lua_State *L) {
     return 3;
 }
 
-static int l_eddsa_sign(lua_State *L) {
+static int l_sign(lua_State *L)
+{
     size_t message_size, secret_key_size;
     const char *message = luaL_checklstring(L, 1, &message_size);
     char *secret_key = (char *) luaL_optlstring(L, 2, NULL, &secret_key_size);
@@ -187,58 +184,17 @@ static int l_eddsa_sign(lua_State *L) {
     char public_key[32];
 
     luaL_argcheck(L, secret_key_size == 0 || secret_key_size == 32, 2,
-        "#secret_key must be 32");
-    if (!secret_key) {
-        secret_key = (char *) l_malloc(L, 32);
-        PRNG(secret_key, 32);
-        free_secret_key = 1;
-    }
-    crypto_sign_public_key((uint8_t *) public_key,
-        (const uint8_t *) secret_key);
-    crypto_sign((uint8_t *) signature, (const uint8_t *) secret_key,
-        (const uint8_t *) public_key, (const uint8_t *) message, message_size);
-
-    lua_pushlstring(L, signature, 64);
-    lua_pushlstring(L, public_key, 32);
-    lua_pushlstring(L, secret_key, 32);
-    if (free_secret_key) free(secret_key);
-    return 3;
-}
-
-static int l_eddsa_check(lua_State *L) {
-    size_t message_size, signature_size, public_key_size;
-    const char *message = luaL_checklstring(L, 1, &message_size);
-    const char *signature = luaL_checklstring(L, 2, &signature_size);
-    const char *public_key = luaL_checklstring(L, 3, &public_key_size);
-
-    luaL_argcheck(L, signature_size == 64, 2, "#signature must be 64");
-    luaL_argcheck(L, public_key_size == 32, 3, "#public_key must be 32");
-
-    lua_pushboolean(L, crypto_check((const uint8_t *) signature,
-        (const uint8_t *) public_key, (const uint8_t *) message,
-        message_size) == 0);
-    return 1;
-}
-
-static int l_ed25519_sign(lua_State *L) {
-    size_t message_size, secret_key_size;
-    const char *message = luaL_checklstring(L, 1, &message_size);
-    char *secret_key = (char *) luaL_optlstring(L, 2, NULL, &secret_key_size);
-    char free_secret_key = 0;
-    char signature[64];
-    char public_key[32];
-
-    luaL_argcheck(L, secret_key_size == 0 || secret_key_size == 32, 2,
-        "#secret_key must be 32");
+                  "#secret_key must be 32");
     if (!secret_key) {
         secret_key = (char *) l_malloc(L, 32);
         PRNG(secret_key, 32);
         free_secret_key = 1;
     }
     crypto_ed25519_public_key((uint8_t *) public_key,
-        (const uint8_t *) secret_key);
+                              (const uint8_t *) secret_key);
     crypto_ed25519_sign((uint8_t *) signature, (const uint8_t *) secret_key,
-        (const uint8_t *) public_key, (const uint8_t *) message, message_size);
+                        (const uint8_t *) public_key,
+                        (const uint8_t *) message, message_size);
 
     lua_pushlstring(L, signature, 64);
     lua_pushlstring(L, public_key, 32);
@@ -247,7 +203,8 @@ static int l_ed25519_sign(lua_State *L) {
     return 3;
 }
 
-static int l_ed25519_check(lua_State *L) {
+static int l_check(lua_State *L)
+{
     size_t message_size, signature_size, public_key_size;
     const char *message = luaL_checklstring(L, 1, &message_size);
     const char *signature = luaL_checklstring(L, 2, &signature_size);
@@ -257,12 +214,13 @@ static int l_ed25519_check(lua_State *L) {
     luaL_argcheck(L, public_key_size == 32, 3, "#public_key must be 32");
 
     lua_pushboolean(L, crypto_ed25519_check((const uint8_t *) signature,
-        (const uint8_t *) public_key, (const uint8_t *) message,
-        message_size) == 0);
+                    (const uint8_t *) public_key, (const uint8_t *) message,
+                    message_size) == 0);
     return 1;
 }
 
-static int l_memcmp(lua_State *L) {
+static int l_equal(lua_State *L)
+{
     size_t i, a_size, b_size;
     const char *a = luaL_checklstring(L, 1, &a_size);
     const char *b = luaL_checklstring(L, 2, &b_size);
@@ -274,7 +232,8 @@ static int l_memcmp(lua_State *L) {
     return 1;
 }
 
-static int l_random(lua_State *L) {
+static int l_random(lua_State *L)
+{
     size_t size = luaL_checkinteger(L, 1);
     char *buffer;
 
@@ -287,17 +246,15 @@ static int l_random(lua_State *L) {
 }
 
 static const luaL_Reg l_monocypher[] = {
-    { "lock", l_lock },
-    { "unlock", l_unlock },
-    { "blake2b", l_blake2b },
+    { "encrypt", l_encrypt },
+    { "decrypt", l_decrypt },
+    { "blake2", l_blake2 },
     { "sha512", l_sha512 },
     { "argon2", l_argon2 },
-    { "x25519", l_x25519 },
-    { "eddsa_sign", l_eddsa_sign },
-    { "eddsa_check", l_eddsa_check },
-    { "ed25519_sign", l_ed25519_sign },
-    { "ed25519_check", l_ed25519_check },
-    { "memcmp", l_memcmp },
+    { "exchange", l_exchange },
+    { "sign", l_sign },
+    { "check", l_check },
+    { "equal", l_equal },
     { "random", l_random },
     { NULL, NULL }
 };
@@ -305,7 +262,8 @@ static const luaL_Reg l_monocypher[] = {
 #ifdef __cplusplus
 extern "C" {
 #endif
-int luaopen_monocypher(lua_State *L) {
+int luaopen_monocypher(lua_State *L)
+{
     luaL_newlib(L, l_monocypher);
     lua_pushstring(L, "Monocypher 3.1.3");
     lua_setfield(L, -2, "_VERSION");
